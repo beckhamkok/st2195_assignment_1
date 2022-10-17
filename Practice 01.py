2a57209a87eed106b2c0f278d26737c1973cf786
beckhamkok <114043434+beckhamkok@users.noreply.github.com>
2022-10-17 12:59:21 +0800

    Update and rename spyder/spyder.py to Practice 01.py

--- Practice 01.py
+++ Practice 01.py
@@ -1,6 +1,8 @@
 ## Practice Assignment 1
 ## A Python script that prints all integers between 1 and 10.
 
+print(1,2,3,4,5,6,7,8,9,10)
+
 ## For loop printing 1 to 10
 for i in range(1,11):
     print(i)
